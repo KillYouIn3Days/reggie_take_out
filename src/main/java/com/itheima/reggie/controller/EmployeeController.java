1773308004049526f2a4bd37556e7e384297753f
WilsonEdwards <860586779@qq.com>
2022-05-24 18:51:57 +0800

    login and logout

--- src/main/java/com/itheima/reggie/controller/EmployeeController.java
+++ src/main/java/com/itheima/reggie/controller/EmployeeController.java
@@ -1,11 +1,20 @@
 package com.itheima.reggie.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.itheima.reggie.common.R;
+import com.itheima.reggie.entity.Employee;
 import com.itheima.reggie.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * <p>
@@ -21,4 +30,43 @@ public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
+
+    /**
+     * 员工登录
+     *
+     * @param request  为了存入session
+     * @param employee 把传来的参数封装成employee对象，
+     *                 username，
+     *                 password，
+     *                 传入的参数名必须和Employee类里的属性名一致，才能匹配上
+     * @return
+     */
+    @PostMapping("/login")
+    public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee) {
+        String username = employee.getUsername();
+        String password = employee.getPassword();
+
+        Employee emp = employeeService.getOne(new QueryWrapper<Employee>().eq("username", username));
+
+        if (emp == null) {
+            return R.error("用户名或密码错误");
+        }
+
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        boolean matches = passwordEncoder.matches(password, emp.getPassword());
+        if (!matches) {
+            return R.error("用户名或密码错误");
+        }
+        if (emp.getStatus() == 0) {
+            return R.error("账号已禁用");
+        }
+        request.getSession().setAttribute("employee", emp.getId());
+        return R.success(emp);
+    }
+
+    @PostMapping("/logout")
+    public R<String> logout(HttpServletRequest request) {
+        request.getSession().removeAttribute("employee");
+        return R.success("退出成功");
+    }
 }
